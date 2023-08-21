c690ebf2c91a71872d76a9090a8f24a7bf83c7b3
Decomori <tskim275@gmail.com>
2023-08-21 19:30:52 +0900

    아이템 링크 추가

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,4 +1,6 @@
 module.exports = {
     'mapleLeap': 'https://ssl.nexon.com/s2/game/maplestory/renewal/common/world_icon/icon_1.png', //메이플 잎
-    'guildIgnoreSkill': 'http://avatar.maplestory.nexon.com/SkillIcon/KFGDLHPBOD.png' //길드 방무 스킬
+    'guildIgnoreSkill': 'http://avatar.maplestory.nexon.com/SkillIcon/KFGDLHPBOD.png', //길드 방무 스킬
+    '뱃지오브준나' : 'http://avatar.maplestory.nexon.com/ItemIcon/KEOKJGHI.png' , //일요일 뱃지
 }
+
