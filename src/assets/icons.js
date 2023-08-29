2bf385dd06c32faf41b41e9d04d2cc1f04077a47
Decomori <tskim275@gmail.com>
2023-08-29 15:29:10 +0900

    아이콘 이미지 2종 추가(솔 에르다, 조각)

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -2,5 +2,7 @@ module.exports = {
     'mapleLeap': 'https://ssl.nexon.com/s2/game/maplestory/renewal/common/world_icon/icon_1.png', //메이플 잎
     'guildIgnoreSkill': 'http://avatar.maplestory.nexon.com/SkillIcon/KFGDLHPBOD.png', //길드 방무 스킬
     '뱃지오브준나' : 'http://avatar.maplestory.nexon.com/ItemIcon/KEOKJGHI.png' , //일요일 뱃지
+    'solErdaPiece' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKG.png', //솔 에르다 조각
+    'solErda' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKH.png', //솔 에르다
 }
 
