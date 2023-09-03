028be58fc6a20de57f56221b93151471ebdb0806
Decomori <tskim275@gmail.com>
2023-09-03 17:16:05 +0900

    츄츄 부스터 아이콘 추가

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -4,5 +4,7 @@ module.exports = {
     '뱃지오브준나' : 'http://avatar.maplestory.nexon.com/ItemIcon/KEOKJGHI.png' , //일요일 뱃지
     'solErdaPiece' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKG.png', //솔 에르다 조각
     'solErda' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKH.png', //솔 에르다
+    'chuchuBooster' : 'https://avatar.maplestory.nexon.com/ItemIcon/KHJBNBLD.png', //츄츄 부스터
+    
 }
 
