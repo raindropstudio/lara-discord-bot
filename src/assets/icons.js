32a86ff0a297a7fc70c46469711105b0245b9346
Decomori <tskim275@gmail.com>
2023-09-04 17:17:56 +0900

    극성비 아이콘 추가

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -4,7 +4,7 @@ module.exports = {
     '뱃지오브준나' : 'http://avatar.maplestory.nexon.com/ItemIcon/KEOKJGHI.png' , //일요일 뱃지
     'solErdaPiece' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKG.png', //솔 에르다 조각
     'solErda' : 'http://avatar.maplestory.nexon.com/ItemIcon/KBPCCCKH.png', //솔 에르다
-    'chuchuBooster' : 'https://avatar.maplestory.nexon.com/ItemIcon/KHJBNBLD.png', //츄츄 부스터
-    
+    'chuchuBooster' : 'http://avatar.maplestory.nexon.com/ItemIcon/KHJBNBLD.png', //츄츄 부스터
+    'ultimateGrowthElixir' : 'http://avatar.maplestory.nexon.com/ItemIcon/KHJBIBME.png', //극한 성장의 비약
 }
 
