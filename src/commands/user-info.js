04e9a8929ec82d886cdc191fd3797ac67c185d4d
Decomori <tskim275@gmail.com>
2023-09-05 17:41:59 +0900

    명령어: /검색 기능에 대한 오류 예외 추가

--- src/commands/user-info.js
+++ src/commands/user-info.js
@@ -18,28 +18,34 @@ module.exports = {
 	async execute(interaction) {
 		commandLogger.logCommandUsage(interaction); //로그 기록
 		const nickname = interaction.options.getString('닉네임');
-		const userInfo = await getUserInfo(nickname);
+		
+		try {
+			const userInfo = await getUserInfo(nickname);
 
-		//console.log('Image URL:', userInfo.characterImg); //캐릭터 이미지 URL 검사
+			const embed = new EmbedBuilder()
+				.setAuthor({ name: '\u200B', iconURL: userInfo.serverImg})
+				.setTitle(`**「   ${nickname}   」**`)
+				.setColor(colors.primary)
+				.setThumbnail(userInfo.characterImg)
+				.setDescription(`> ${userInfo.job}\n`, inline = false)
+				.addFields(
+					{ name: '【 레벨 】', value: `\`${userInfo.level}\``, inline: true },
+					{ name: '【 경험치 】', value: ` \`${calculateExpPercentage(userInfo.level, userInfo.experience)}% (${userInfo.experience})\``, inline: false },
+					{ name: '【 인기도 】', value: `\`${userInfo.pop}\``, inline: true },
+					{ name: '【 길드 】', value: userInfo.guild && userInfo.guild.length > 0 ? `\`${userInfo.guild}\`` : '없음', inline: true })
+				.setFooter({ text: randomQuote.getRandomQuote(), iconURL: icons.mapleLeap });
 
-		const embed = new EmbedBuilder()
-			.setAuthor({ name: '\u200B', iconURL: userInfo.serverImg})
-			.setTitle(`**「   ${nickname}   」**`)
-			.setColor(colors.primary)
-			.setThumbnail(userInfo.characterImg)
-			.setDescription(`> ${userInfo.job}\n`, inline = false)
-			.addFields(
-				//{ name: '닉네임', value: userInfo.nickname, inline: true },
-				//{ name: '직업', value: userInfo.job, inline: true },
-				//{ name: '\u200B', value: '\u200B', inline: false },
-				{ name: '【 레벨 】', value: `\`${userInfo.level}\``, inline: true },
-				//{ name: '\u200B', value: `\`${calculateExpPercentage(userInfo.level, userInfo.experience)} %\``, inline: true },				
-				{ name: '【 경험치 】', value: ` \`${calculateExpPercentage(userInfo.level, userInfo.experience)}% (${userInfo.experience})\``, inline: false },
-				{ name: '【 인기도 】', value: `\`${userInfo.pop}\``, inline: true },
-				{ name: '【 길드 】', value: userInfo.guild && userInfo.guild.length > 0 ? `\`${userInfo.guild}\`` : '없음', inline: true })			
-			//.setImage('https://ssl.nexon.com/s2/game/maplestory/renewal/common/world_icon/icon_13.png', inline = true)
-			.setFooter({ text: randomQuote.getRandomQuote(), iconURL: icons.mapleLeap });
-		await interaction.reply({ embeds: [embed] });
-		console.log(embed);
+			await interaction.reply({ embeds: [embed] });
+
+		} catch (error) {
+			commandLogger.logCommandIssue(interaction, error); //로그 기록
+			// 오류가 발생한 경우 사용자에게 알려줄 임베드 생성
+			const embed = new EmbedBuilder()
+				.setColor(colors.error)
+				.setTitle("입력한 닉네임의 용사님을 찾을 수 없어요!")
+				.setDescription("메이플스토리 랭킹 서버에 등록되어 있지 않거나, 잘못된 닉네임을 입력하셨어요. 다시 입력해주세요!");
+
+			await interaction.reply({ embeds: [embed] });
+		}
 	},
 };
