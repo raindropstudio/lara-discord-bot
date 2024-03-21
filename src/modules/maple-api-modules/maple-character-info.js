49f63c966b69b0d96cc0991ebae414cb04491227
Decomori <tskim275@gmail.com>
2024-03-22 03:37:06 +0900

    캐릭터 정보 기본 조회 모듈

---
+++ src/modules/maple-api-modules/maple-character-info.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const axios = require("axios");
+const api_token = process.env.MAPLE_API_TOKEN;
+
+async function characterInfo(ocid) {
+  const url = `https://open.api.nexon.com/maplestory/v1/character/basic?ocid=${ocid}&date=2024-03-20`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "x-nxopen-api-key": api_token,
+      },
+    });
+
+    const characterInfo = response.data;
+    //console.log(characterInfo);
+
+    return characterInfo;
+  } catch (error) {
+    console.error("API 호출 중 오류가 발생했습니다: ", error.message);
+    if (error.response) {
+      // 서버 응답에 포함된 추가 정보를 출력
+      console.log(characterName);
+      console.log(error.response.data);
+      return error;
+    }
+  }
+}
+
+module.exports = characterInfo;
