984ddfc5328672883ffe28f95fc0d8b4c56eb3d1
Decomori <tskim275@gmail.com>
2024-03-29 17:52:21 +0900

    캐릭터 스탯 모듈 기본기능 구현

---
+++ src/modules/maple-api-modules/character-info-modules/maple-character-stats.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const getCharacterOCID = require('./maple-ocid');
+const axios = require('axios');
+const api_token = process.env.MAPLE_API_TOKEN;
+
+const characterStats = async(characterName, date = '') => {
+  const ocid = await getCharacterOCID(characterName);
+  const url = `https://open.api.nexon.com/maplestory/v1/character/stat?ocid=${ocid}${date ? "&date=" + date : ""}`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'x-nxopen-api-key': api_token,
+      },
+    });
+    const characterStatsData = response.data;
+    //console.log(characterStatsData);
+    return characterStatsData;
+  } catch (error) {
+    console.error("캐릭터 스탯 관련 정보를 호출 중 오류가 발생했습니다: ", error.message);
+    if (error.response) {
+      console.log(characterName);
+      console.log(error.response.data);
+      console.log(url);
+      return error;
+    }
+  }
+}
+module.exports = characterStats;
--- src/test/module-test.js
+++ src/test/module-test.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 const getCharacterInfo = require("../modules/maple-api-modules/character-info-modules/maple-character-info");
 const getCharacterOCID = require("../modules/maple-api-modules/character-info-modules/maple-ocid");
+const getCharacterStats = require('../modules/maple-api-modules/character-info-modules/maple-character-stats');
+const characterStats = require("../modules/maple-api-modules/character-info-modules/maple-character-stats");
 
 async function test() {
   try {
     const characterOCID = await getCharacterOCID("섭주");
     const characterInfo = await getCharacterInfo(characterOCID);
+    const characterStats = await getCharacterStats("섭주");
+    //const getPowerStat = characterStats.final_stats.find(stat => stat.stat_name === "전투력");
+
     console.log(characterInfo.character_level);
+    console.log(characterStats);
   } catch (error) {
     console.error("테스트 중 오류 발생:", error.message);
+    console.log(characterStats.url);
   }
 }
 
-test();
+test("섭주");
